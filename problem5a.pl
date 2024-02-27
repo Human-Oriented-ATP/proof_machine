4116a517679c060d9faffe9dfa1ec0f35c890f03
wtgowers <wtg10@cam.ac.uk>
2024-02-27 21:40:59 +0000

    Add files via upload

---
+++ problem5a.pl
@@ -0,0 +1,12 @@
+g(3,1,2).
+r(4,3).
+r(5,1).
+r(6,2).
+g(A,7,A).
+g(A,A,7).
+r(B,A) :- r(A,B).
+g(7,A,B) :- r(A,B).
+g(E,A,F) :- g(D,A,B), g(E,D,C), g(F,B,C).
+g(E,D,C) :- g(D,A,B), g(F,B,C), g(E,A,F).
+g(xg(A,B),A,B).
+:- g(4,6,5).
