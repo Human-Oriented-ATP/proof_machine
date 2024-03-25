cebf37fcdef464b6df5e4a23982b93e7824d1a62
Jonas Bayer <jonas.bayer999@gmail.com>
2024-03-25 16:55:01 +0000

    Add datatype for assigning values to the disjoint sets

---
+++ web-proof-machine/src/util/DisjointSetWithAssignment.ts
@@ -0,0 +1,26 @@
+import { DisjointSet } from "./DisjointSet";
+
+export class DisjointSetWithAssignment<T, S> extends DisjointSet<T> {
+    private assignments: Map<T, S>;
+
+    constructor() {
+        super()
+        this.assignments = new Map()
+    }
+
+    assign(t: T, value: S) {
+        const representative = this.findRepresentative(t)
+        this.assignments.set(representative, value)
+    }
+
+    getAssignedValue(t: T): S | undefined {
+        const representative = this.findRepresentative(t)
+        return this.assignments.get(representative)
+    }
+
+    isAssigned(t: T): boolean {
+        const representative = this.findRepresentative(t)
+        return this.assignments.has(representative)
+    }
+
+}
