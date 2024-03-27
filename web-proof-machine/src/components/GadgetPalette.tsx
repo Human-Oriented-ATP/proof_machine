2475871f179e11a56873fd1a03dc15f2cd33f51a
ART0 <18333981+0Art0@users.noreply.github.com>
2024-03-27 11:41:07 +0000

    Restructuring content of `GadgetPalette.tsx`

--- web-proof-machine/src/components/GadgetPalette.tsx
+++ web-proof-machine/src/components/GadgetPalette.tsx
@@ -10,35 +10,30 @@ export interface GadgetPaletteProps {
 interface InsertGadgetButtonProps extends React.PropsWithChildren<{}> {
     gadget: AbstractGadgetProps
     createNewGadget: any
+    axiomIdx : number
 }
 
-export function InsertGadgetButton({ gadget, createNewGadget, children }: InsertGadgetButtonProps): 
+export function InsertGadgetButton({ gadget, createNewGadget, axiomIdx }: InsertGadgetButtonProps): 
     JSX.Element {
+    const gadgetProps: GadgetDisplayProps = {
+        ...gadget,
+        isAxiom: true,
+        id: "axiom" + axiomIdx
+        }
     return <div className="insertGadgetButton" onMouseDown={(e) => createNewGadget(e, gadget)}>
-        {children}
+        {<Gadget {...gadgetProps} />}
     </div>
 }
 
 export function GadgetPalette({ ...props }: GadgetPaletteProps) {
-    let id = 0;
-
-    function makeAxiomGadget(axiom: AbstractGadgetProps): JSX.Element {
-        const gadgetProps: GadgetDisplayProps = {
-            ...axiom,
-            isAxiom: true,
-            id: "axiom" + id
-        }
-        id++
-        return <Gadget {...gadgetProps}></Gadget>
-    }
-
     return (
         <Panel position='top-center'>
             <div className="gadgetPalette">
-                {props.axioms.map(axiom =>
-                    <InsertGadgetButton gadget={axiom} createNewGadget={props.createNewGadget}>
-                        {makeAxiomGadget(axiom)}
-                    </InsertGadgetButton>)}
+                {props.axioms.map((axiom, idx) =>
+                    <InsertGadgetButton 
+                        gadget={axiom}
+                        createNewGadget={props.createNewGadget}
+                        axiomIdx={idx} />)}
             </div>
         </Panel>
     )
