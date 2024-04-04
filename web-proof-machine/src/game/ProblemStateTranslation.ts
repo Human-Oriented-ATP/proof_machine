f68dea7edbc1b4956984b4b08cf51a2bd59cf752
ART0 <18333981+0Art0@users.noreply.github.com>
2024-04-04 14:42:19 +0100

    Function to check if a term is well-formed

--- web-proof-machine/src/game/ProblemStateTranslation.ts
+++ web-proof-machine/src/game/ProblemStateTranslation.ts
@@ -1,5 +1,6 @@
 import { AbstractGadgetProps, AbstractGameProps, AbstractNodeProps, Axiom, Color, HolePosition, HoleProps, InternalConnection, NodePosition, ProblemState, Term } from "./Primitives";
 import { getIndexFor } from './TermEnumeration';
+import { TermIndex, hashTerm } from "./TermIndex";
 import { TermUnifier } from "./TermUnifier";
 
 //TODO: Move to `Primitives`
@@ -16,8 +17,29 @@ function createConnections(locatedTerms: LocatedNode[]): InternalConnection[] {
     return [];
 }
 
+function validateTerm(termIndex: TermIndex, term: Term) : void {
+    if ('variable' in term) {
+        throw new Error("The nodes in a problem state cannot be variables.");
+    } else {
+        try {
+            const color: Color = term.label as Color;
+        } catch {
+            throw new Error(`The label of a node is expected to be a \`Color\`; received ${term.label} instead.`);
+        }
+        const functionNodeIndex = term.args.findIndex(arg => !('variable' in arg))
+        // if  (!(term.args.every((arg, idx) => idx === functionNodeIndex || ('variable' in arg)))) {
+            // this case is probably not necessary after all
+        // }
+        if (!(term.args.every(arg => ('variable' in arg) ? 
+        termIndex.termHasVariable(hashTerm(term.args[functionNodeIndex]), arg.variable) : true))) {
+            throw new Error("Holes that represent non-variable terms must depend on all the other variables in the node.")
+        }
+    }
+}
+
 function translateProblemState(termUnifier: TermUnifier, ps: ProblemState): AbstractGameProps {
     function termToNodeProps(term: Term): AbstractNodeProps {
+        validateTerm(termUnifier, term)
         if ('variable' in term) {
             throw new Error("The axioms in the problem state cannot be variables.")
         } else {
