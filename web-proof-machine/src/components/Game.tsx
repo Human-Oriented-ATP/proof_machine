83c6e538dcb7cba5e99e6d4bf9d7d5fa25003930
Jonas Bayer <jonas.bayer999@gmail.com>
2024-04-04 14:38:40 +0100

    Split logical state and diagram state into separate components

--- web-proof-machine/src/components/Game.tsx
+++ web-proof-machine/src/components/Game.tsx
@@ -1,44 +1,17 @@
-import { useCallback, useMemo, useState } from 'react';
-import ReactFlow, {
-    Controls,
-    useNodesState,
-    useEdgesState,
-    addEdge,
-    NodeTypes,
-    Connection as ReactFlowConnection,
-    useReactFlow,
-    Node as ReactFlowNode,
-    EdgeTypes,
-    useStore,
-    ReactFlowState,
-} from 'reactflow';
-import { GadgetFlowNode, getFlowNodeTerms } from './GadgetFlowNode';
-import { GadgetPalette, GadgetPaletteProps } from './GadgetPalette';
-import { MultiEdge } from './MultiEdge';
+import { ReactFlowProvider } from "reactflow";
+import { Diagram } from "./Diagram";
+import { Axiom, HoleValueAssignment, makeAxiomsFromJSONObject } from "../game/GameLogic";
 import problemData from "../game/examples/problem1.json"
+import { Term, TermAssignment, hashTerm, makeTermWithFreshVariables } from "../game/Term";
+import { useMemo, useRef, useState } from "react";
+import { Equation, unifyEquations } from "../game/Unification";
+import { HoleValue } from "../game/Primitives";
+import { useIdGenerator } from "../util/IdGeneratorHook";
+import { GadgetFlowNodeProps } from "./GadgetFlowNode";
 
-import 'reactflow/dist/style.css';
-import '../flow.css'
-import { Axiom, HoleValueAssignment, getTermOfHandle, makeAxiomGadget, makeAxiomsFromJSONObject } from '../game/GameLogic';
-import { Equation, unifyEquations } from '../game/Unification';
-import { Term, TermAssignment, hashTerm, makeTermWithFreshVariables } from '../game/Term';
-import { useIdGenerator } from '../util/IdGeneratorHook';
-import { useMap } from 'usehooks-ts';
-import { HoleValue } from '../game/Primitives';
+type EnumerationMap = Map<Term, number>
 
-const nodeTypes: NodeTypes = { 'gadgetFlowNode': GadgetFlowNode }
-const edgeTypes: EdgeTypes = { 'multiEdge': MultiEdge }
-
-const equationSelector = (state: ReactFlowState) => {
-    return state.edges.map(edge => edge.data)
-}
-
-function compare(prev: Equation[], next: Equation[]) {
-    return prev.length === next.length
-
-}
-
-function getNextFreeHoleValue(offset: number, enumerationMap: Map<number, HoleValue>): number {
+function getNextFreeHoleValue(offset: number, enumerationMap: EnumerationMap): number {
     const values = Array.from(enumerationMap.values())
     for (let i = offset; true; i++) {
         if (!values.includes(i)) {
@@ -47,21 +20,21 @@ function getNextFreeHoleValue(offset: number, enumerationMap: Map<number, HoleVa
     }
 }
 
-function updateEnumeration(offset: number, immutablePreviousEnumeration: Map<number, HoleValue>,
-    termAssignment: TermAssignment) {
+function updateEnumeration(offset: number, immutablePreviousEnumeration: EnumerationMap,
+    termAssignment: TermAssignment): EnumerationMap {
     let previousEnumeration = new Map(immutablePreviousEnumeration)
     const allAssignedTerms = termAssignment.getAssignedValues()
     for (const term of allAssignedTerms) {
         const hash = hashTerm(term)
         if (!(hash in previousEnumeration.keys())) {
             const value = getNextFreeHoleValue(offset, previousEnumeration)
-            previousEnumeration.set(hash, value)
+            previousEnumeration.set(term, value)
         }
     }
     return previousEnumeration
 }
 
-function toHoleValue(t: Term, enumerationMap: Map<number, HoleValue>): HoleValue {
+function toHoleValue(t: Term, enumerationMap: EnumerationMap): HoleValue {
     if ("variable" in t) {
         throw Error("Cannot get hole value for a variable" + t)
     } else {
@@ -69,8 +42,7 @@ function toHoleValue(t: Term, enumerationMap: Map<number, HoleValue>): HoleValue
             const value = Number(t.label)
             return value
         } else {
-            const hash = hashTerm(t)
-            const lookup = enumerationMap.get(hash)
+            const lookup = enumerationMap.get(t)
             if (lookup) {
                 return lookup
             } else {
@@ -80,7 +52,7 @@ function toHoleValue(t: Term, enumerationMap: Map<number, HoleValue>): HoleValue
     }
 }
 
-function getHoleValueAssignment(enumerationMap: Map<number, HoleValue>, termAssignment: TermAssignment):
+function getHoleValueAssignment(enumerationMap: EnumerationMap, termAssignment: TermAssignment):
     HoleValueAssignment {
     return (t => {
         if ("variable" in t) {
@@ -98,89 +70,49 @@ function getHoleValueAssignment(enumerationMap: Map<number, HoleValue>, termAssi
 
 export function Game() {
     const axioms = makeAxiomsFromJSONObject(problemData)
-    const getGadgetId = useIdGenerator("gadget_")
-    const [nodes, setNodes, onNodesChange] = useNodesState([]);
-    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-    const { getNode, screenToFlowPosition } = useReactFlow();
-    const equationsFromStore = useStore(equationSelector, compare)
+    const enumerationOffset = 10 // later: get this from axioms
+    const generateGadgetId = useIdGenerator("gadget_")
 
-    const init = new Map()
-    const [enumerationMap, setEnumerationMap] = useState<Map<number, HoleValue>>(init)
+    const [equations, setEquations] = useState<Equation[]>([])
+    const enumeration = useRef<EnumerationMap>(new Map())
 
-    const offset = 5
+    const [holeValueAssignment, eqSatisfied] = useMemo(() => {
+        const [assignment, eqSatisfied] = unifyEquations(equations)
+        enumeration.current = updateEnumeration(enumerationOffset, enumeration.current, assignment)
+        const holeValueAssignment = getHoleValueAssignment(enumeration.current, assignment)
+        return [holeValueAssignment, eqSatisfied]
+    }, [equations])
 
-    const [assignment, eqSatisfied] = useMemo(() => {
-        console.log("...unifying...")
-        const [assignment, eqSatisfied] = unifyEquations(equationsFromStore)
-        console.log("...calculated assignment: ", assignment)
-        setEnumerationMap(map => updateEnumeration(offset, map, assignment))
-        return [assignment, eqSatisfied]
-    }, [equationsFromStore, setEnumerationMap])
-
-    // Need to useMemo so this only happens when the values actually change 
-    const updateDiagramElements = useMemo(() => {
-        console.log("...in 2nd memo function...")
-        console.log("enumMap", enumerationMap)
-        const holeValueAssignment = getHoleValueAssignment(enumerationMap, assignment)
-        setNodes(nodes => nodes.map(node => {
-            const newData = { ...node.data, assignment: holeValueAssignment }
-            return { ...node, data: newData }
-        }))
-        setEdges(edges => edges.map(edge => {
-            const isSatisfied = eqSatisfied.get(edge.data)
-            return { ...edge, animated: isSatisfied ? false : true }
-        }))
-    }, [assignment, eqSatisfied, enumerationMap, setNodes, setEdges]);
-
-
-    function addConnection(params: ReactFlowConnection): void {
-        setEdges((edges) => {
-            const sourceTerms: Term[] = getFlowNodeTerms(getNode(params.source!)!.data)
-            const targetTerms: Term[] = getFlowNodeTerms(getNode(params.target!)!.data)
-            const sourceTerm: Term = getTermOfHandle(params.sourceHandle!, sourceTerms)!
-            const targetTerm: Term = getTermOfHandle(params.targetHandle!, targetTerms)!
-            const equation: Equation = [sourceTerm, targetTerm]
-            return addEdge({ ...params, type: 'multiEdge', data: equation }, edges)
-        });
+    function addEquation(newEquation: Equation) {
+        setEquations(equations => [...equations, newEquation])
     }
 
-    const onConnect = useCallback(addConnection, [getNode, setEdges]);
+    function deleteEquation(equationToBeDeleted: Equation) {
+        setEquations(equations => equations.filter(equation =>
+            JSON.stringify(equation) !== JSON.stringify(equationToBeDeleted)))
+    }
 
-    function createNewGadget(axiom: Axiom, e: React.MouseEvent): void {
-        const id = getGadgetId()
+    function makeNewGadget(axiom: Axiom): GadgetFlowNodeProps {
+        const id = generateGadgetId()
         const inputTerms = axiom.hypotheses.map(hyp => makeTermWithFreshVariables(hyp, id))
         const outputTerm = makeTermWithFreshVariables(axiom.conclusion, id)
-        const emptyAssignment: HoleValueAssignment = (t: Term) => ""
-        const newNode: ReactFlowNode =
-        {
-            id: id,
-            type: 'gadgetFlowNode',
-            position: screenToFlowPosition({
-                x: e.clientX, y: 450
-            }),
-            data: { inputs: inputTerms, output: outputTerm, id, assignment: emptyAssignment }
+        const gadget: GadgetFlowNodeProps = {
+            inputs: inputTerms,
+            output: outputTerm,
+            id,
+            assignment: holeValueAssignment
         }
-        setNodes((nodes) => nodes.concat(newNode));
+        return gadget
     }
 
-    const panelProps: GadgetPaletteProps = {
-        axioms: axioms,
-        makeAxiomGadget: makeAxiomGadget,
-        makeGadget: createNewGadget
-    }
-
-    return (
-        <ReactFlow
-            nodes={nodes}
-            edges={edges}
-            onNodesChange={onNodesChange}
-            onEdgesChange={onEdgesChange}
-            onConnect={onConnect}
-            edgeTypes={edgeTypes}
-            nodeTypes={nodeTypes}
-        >
-            <GadgetPalette {...panelProps} />
-            <Controls />
-        </ReactFlow>
-    )
+    return <ReactFlowProvider>
+        <Diagram
+            axioms={axioms}
+            makeNewGadget={makeNewGadget}
+            addEquation={addEquation}
+            deleteEquation={deleteEquation}
+            isSatisfied={eqSatisfied}
+            holeValueAssignment={holeValueAssignment}
+        ></Diagram>
+    </ReactFlowProvider>
 }
