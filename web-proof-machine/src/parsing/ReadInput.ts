b0d538535ba90b69c2beca1b77661da9ee1519b8
ART0 <18333981+0Art0@users.noreply.github.com>
2024-04-16 15:08:47 +0100

    Function for reading input file

---
+++ web-proof-machine/src/parsing/ReadInput.ts
@@ -0,0 +1,10 @@
+import { InitializationData } from "../game/Initialization"
+import { buildAst} from "./Semantics"
+import { readFileSync } from "fs"
+
+const problemsFolder = "../../../problems/"
+
+export function readProblemFile(fileName: string): InitializationData {
+    const text = readFileSync(`${problemsFolder}/${fileName}.pl`).toString()
+    return buildAst(text)
+}
