829722ad9b1b24498e4c72f76888101a07955ee4
Jonas Bayer <jonas.bayer999@gmail.com>
2024-04-19 09:59:06 +0200

    Switch framework to Next.js

---
+++ web-proof-machine/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next"
+import "../index.css"
+
+export const metadata: Metadata = {
+    title: 'Proof Machine',
+    description: '',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return <html lang="en">
+        <body>
+            <noscript>You need to enable JavaScript to run this app.</noscript>
+            <div id="root">{children}</div>
+        </body>
+    </html>
+}
