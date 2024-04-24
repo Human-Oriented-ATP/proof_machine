78f8af794dc30cd18f274259f8a6d0b5bce23660
Jonas Bayer <jonas.bayer999@gmail.com>
2024-04-24 10:50:11 +0200

    Move Problem files

---
+++ web-proof-machine/problems/problem1.pl
@@ -0,0 +1,17 @@
+g(1,2,3). % this is a comment
+b(4,2,3).
+r(1,2).
+% this is a comment
+% on a standalone line
+r(A,B) :- g(B,A,C).
+r(A,B) :- b(A,B,C).
+r(A,B) :- r(A,C), r(C,B).
+g(xg(A,B),A,B).
+b(xb(A,B),A,B).
+w(A,B) :- g(A,C,D), g(B,D,C).
+w(A,B) :- b(A,C,D), b(B,D,C).
+r(A,B) :- w(A,B).
+r(A,B) :- w(B,A).
+r(A,B) :- r(A,C), r(A,D), b(B,C,D).
+r(A,A).
+:- r(3,4).
