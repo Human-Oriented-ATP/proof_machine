45064bc71493143f7d0e5adacc3868bb10e6d4d7
ART0 <18333981+0Art0@users.noreply.github.com>
2024-04-26 14:58:12 +0100

    Trim input file string

--- web-proof-machine/src/app/game/[slug]/page.tsx
+++ web-proof-machine/src/app/game/[slug]/page.tsx
@@ -13,7 +13,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
     const problemFile = params.slug + ".pl"
     const problemData = await fs.readFile(process.cwd() + "/problems/" + problemFile, "utf-8")
 
-    const initData = buildAst(problemData)
+    const initData = buildAst(problemData.trim())
 
     return <Game initData={initData} />
 }
