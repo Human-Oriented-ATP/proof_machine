bd016e30c5e6580546ff47220e2704f3a218f43f
Jonas Bayer <jonas.bayer999@gmail.com>
2024-05-06 09:00:26 +0100

    Refactor: Remove unnecessary callbacks in hooks

--- web-proof-machine/lib/util/CompletionCheckHook.tsx
+++ web-proof-machine/lib/util/CompletionCheckHook.tsx
@@ -7,43 +7,42 @@ interface CompletionCheckProps {
     edges: Edge[]
     nodes: Node[]
 }
+function onlyContainsValidConnections(edges: Edge[]) {
+    return edges.every(edge => !edge.animated);
+}
 
 export function useCompletionCheck(props: CompletionCheckProps) {
     const { getNode } = useReactFlow();
 
-    const onlyContainsValidConnections = useCallback((edges: Edge[]) => {
-        return edges.every(edge => !edge.animated);
-    }, [])
+    useEffect(() => {
+        function hasUnconnectedInputHandle(node: Node): boolean {
+            const numberOfIncomingEdges = getIncomers(node, props.nodes, props.edges).length
+            const numberOfInputTerms = node.data.inputs.length
+            return numberOfInputTerms !== numberOfIncomingEdges
+        }
 
-    const hasUnconnectedInputHandle = useCallback((node: Node): boolean => {
-        const numberOfIncomingEdges = getIncomers(node, props.nodes, props.edges).length
-        const numberOfInputTerms = node.data.inputs.length
-        return numberOfInputTerms !== numberOfIncomingEdges
-    }, [])
-
-    const isCompleted = useCallback((): boolean => {
-        const goalNode = getNode("goal_gadget")!
-        let observedComponent: Node<any>[] = []
-        let currentLayer = [goalNode]
-        while (true) {
-            for (const node of currentLayer) {
-                if (hasUnconnectedInputHandle(node)) {
-                    return false
+        function isCompleted(): boolean {
+            const goalNode = getNode("goal_gadget")!
+            let observedComponent: Node<any>[] = []
+            let currentLayer = [goalNode]
+            while (true) {
+                for (const node of currentLayer) {
+                    if (hasUnconnectedInputHandle(node)) {
+                        return false
+                    }
+                }
+                observedComponent = observedComponent.concat(currentLayer)
+                const nextLayer = currentLayer.map(node => getIncomers(node, props.nodes, props.edges)).flat()
+                if (nextLayer.length === 0) {
+                    break
+                } else {
+                    currentLayer = nextLayer
                 }
             }
-            observedComponent = observedComponent.concat(currentLayer)
-            const nextLayer = currentLayer.map(node => getIncomers(node, props.nodes, props.edges)).flat()
-            if (nextLayer.length === 0) {
-                break
-            } else {
-                currentLayer = nextLayer
-            }
+            const edgesInComponent = getConnectedEdges(observedComponent, props.edges)
+            return onlyContainsValidConnections(edgesInComponent)
         }
-        const edgesInComponent = getConnectedEdges(observedComponent, props.edges)
-        return onlyContainsValidConnections(edgesInComponent)
-    }, [getNode, props])
 
-    useEffect(() => {
         props.setProblemSolved(isCompleted())
-    }, [isCompleted])
+    }, [getNode])
 }
--- web-proof-machine/lib/util/CustomDeleteHook.tsx
+++ web-proof-machine/lib/util/CustomDeleteHook.tsx
@@ -13,28 +13,27 @@ interface CustomDeleteProps {
 export function useCustomDelete(props: CustomDeleteProps) {
     const backspacePressed = useKeyPress("Backspace")
 
-    const isSelectedAndDeletable = useCallback((node: Node) => {
-        return node.selected && props.isDeletable(node.id!);
-    }, [])
-
-    const getAdjacentEdges = useCallback((node: Node) => {
-        const adjacentEdges = props.edges.filter(e => e.source === node.id || e.target === node.id)
-        return adjacentEdges
-    }, [])
-
-    const deleteDeletableSelectedNodes = useCallback(() => {
-        const nodesToBeDeleted = props.nodes.filter(node => isSelectedAndDeletable(node))
-        const edgesToBeDeleted = nodesToBeDeleted.map(node => getAdjacentEdges(node)).flat()
-        props.deleteEquationsOfEdges(edgesToBeDeleted)
-        const edgeIds = edgesToBeDeleted.map(e => e.id)
-        props.setEdges(edges => edges.filter(edge => !edgeIds.includes(edge.id)))
-        props.setNodes(nodes => nodes.filter(node => !isSelectedAndDeletable(node)))
-    }, [])
-
     useEffect(() => {
+        function isSelectedAndDeletable(node: Node) {
+            return node.selected && props.isDeletable(node.id!);
+        }
+
+        function getAdjacentEdges(node: Node) {
+            const adjacentEdges = props.edges.filter(e => e.source === node.id || e.target === node.id)
+            return adjacentEdges
+        }
+
+        function deleteDeletableSelectedNodes() {
+            const nodesToBeDeleted = props.nodes.filter(node => isSelectedAndDeletable(node))
+            const edgesToBeDeleted = nodesToBeDeleted.map(node => getAdjacentEdges(node)).flat()
+            props.deleteEquationsOfEdges(edgesToBeDeleted)
+            const edgeIds = edgesToBeDeleted.map(e => e.id)
+            props.setEdges(edges => edges.filter(edge => !edgeIds.includes(edge.id)))
+            props.setNodes(nodes => nodes.filter(node => !isSelectedAndDeletable(node)))
+        }
+
         if (backspacePressed) {
             deleteDeletableSelectedNodes()
         }
-    }, [backspacePressed, deleteDeletableSelectedNodes])
-
+    }, [backspacePressed, props])
 }
