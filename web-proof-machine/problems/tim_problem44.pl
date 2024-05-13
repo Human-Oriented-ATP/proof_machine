d6e6081c43154123ed6cc253ccca192ea80a390a
wtgowers <wtg10@cam.ac.uk>
2024-05-13 18:29:24 +0100

    Add files via upload

---
+++ web-proof-machine/problems/tim_problem44.pl
@@ -0,0 +1,16 @@
+y(1,A).
+w(2,1).
+y(3,1).
+g(4,2,3).
+r(S,E) :- c(M,N), b(N,E), w(S,N).
+r(S,E) :- c(M,N), o(N,E), y(S,N).
+p(xp(D),D).
+bl(xbl(D),D).
+w(xw(A,B),A,B).
+r(C,D) :- p(A,D), bl(B,D), g(C,A,B).
+c(P,M) :- w(P,M,N).
+c(P,N) :- w(P,M,N). 
+c(P,R) :- c(P,Q), c(Q,R).
+r(A,C) :- r(A,B), r(B,C).
+r(4,5).
+
