0d2b63661aafc9387aedb83aa43327676c435a8e
wtgowers <wtg10@cam.ac.uk>
2024-05-13 21:31:03 +0100

    Update tim_problem44.pl
    Removed redundant rules

--- web-proof-machine/problems/tim_problem44.pl
+++ web-proof-machine/problems/tim_problem44.pl
@@ -11,12 +11,8 @@ b(xb(A),A).
 o(xo(A),A).
 p(xp(D),D).
 bl(xbl(D),D).
-w(xw(A,B),A,B).
 r(E,F) :- r(A,C), r(B,D), g(E,A,B), g(F,C,D).
 r(C,D) :- p(A,D), bl(B,D), g(C,A,B).
-c(P,M) :- w(P,M,N).
-c(P,N) :- w(P,M,N). 
-c(P,R) :- c(P,Q), c(Q,R).
 r(A,C) :- r(A,B), r(B,C).
 :- r(4,5).
 
