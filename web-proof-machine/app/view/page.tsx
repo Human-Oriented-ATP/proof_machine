6b106dcb9dbc3f72996453d7c1a8078742975121
Jonas Bayer <jonas.bayer999@gmail.com>
2024-05-13 13:27:57 +0100

    Feat: Switch back to statically rendered pages

--- web-proof-machine/app/view/page.tsx
+++ web-proof-machine/app/view/page.tsx
@@ -1,13 +1,8 @@
 import HistorySelector from 'components/HistorySelector';
 import { loadProblemList } from 'lib/game/LoadProblems';
-import { getPlayerId } from 'lib/synchronizeHistory';
 import React from 'react';
 
-export const dynamic = "force-dynamic"
-
 export default async function Page({ params }: { params: { slug: string } }) {
-    const playerId = await getPlayerId()
     const problems = await loadProblemList()
-
-    return <HistorySelector playerId={playerId} problems={problems} />
+    return <HistorySelector problems={problems} />
 }
--- web-proof-machine/components/HistorySelector.tsx
+++ web-proof-machine/components/HistorySelector.tsx
@@ -1,14 +1,24 @@
 "use client"
 
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import ProblemHistoryDisplay from "./ProblemHistoryDisplay"
+import { getPlayerId } from "lib/synchronizeHistory"
 
-export default function HistorySelector({ playerId, problems }: { playerId: string, problems: string[] }) {
+export default function HistorySelector({ problems }: { problems: string[] }) {
+    const [playerId, setPlayerId] = useState("")
     const [problemId, setProblemId] = useState("")
 
     function selectProblem(event: ChangeEvent<HTMLSelectElement>): void {
         setProblemId(event.target.value)
     }
+
+    useEffect(() => {
+        async function loadPlayerId() {
+            const id = await getPlayerId()
+            setPlayerId(id)
+        }
+        loadPlayerId()
+    }, [])
 
     return <div>
         <div className="p-4 text-lg">Displaying history for player with id {playerId}</div>
