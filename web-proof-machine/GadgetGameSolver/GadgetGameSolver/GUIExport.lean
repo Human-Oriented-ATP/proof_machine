9f3854aa05f933e2efdc4e6dc1b9ad9cb086eba6
0art0 <18333981+0art0@users.noreply.github.com>
2024-06-30 20:35:41 +0100

    Adjust y coordinate computation

--- web-proof-machine/GadgetGameSolver/GadgetGameSolver/GUIExport.lean
+++ web-proof-machine/GadgetGameSolver/GadgetGameSolver/GUIExport.lean
@@ -10,17 +10,17 @@ structure GadgetProps where
   inputs : Array Term
   output? : Option Term := none
   isAxiom := false
-deriving Lean.ToJson, Lean.FromJson
+deriving Lean.ToJson, Lean.FromJson, Repr
 
 structure GadgetPropsWithPosition extends GadgetProps where
   x : Nat
   y : Nat
-deriving Lean.ToJson, Lean.FromJson
+deriving Lean.ToJson, Lean.FromJson, Repr
 
 structure Edge where
   source : GadgetId
   target : GadgetId
-deriving Lean.ToJson, Lean.FromJson
+deriving Lean.ToJson, Lean.FromJson, Repr
 
 structure ProofTree.RenderingParams where
   holeWidth : Nat := 25
@@ -51,11 +51,16 @@ end LensNotation
 def mkGadgetId (loc : Array Nat) : String :=
   s!"gadget_{loc}"
 
-partial def ProofTree.exportToGraph : ProofTree → ReaderT ProofTree.RenderingParams (StateM ProofTree.RenderingState) Unit
+def nodeSize (term : Term) : ReaderM ProofTree.RenderingParams Nat := do
+  let params ← read
+  match term with
+    | .var _ => return 0
+    | .app _ args => return 2 * params.nodePadding + params.holeWidth * args.size
+
+partial def ProofTree.exportToGraph : ProofTree → StateT ProofTree.RenderingState (ReaderM ProofTree.RenderingParams) Unit
   | .goal term => do
-    let params ← read
     let state ← get
-    let nodeHeight := 2 * params.nodePadding + term.argsSize
+    let nodeHeight ← nodeSize term
     let gadgetProps : GadgetPropsWithPosition := {
       id := mkGadgetId state.location,
       inputs := #[term],
@@ -79,16 +84,22 @@ partial def ProofTree.exportToGraph : ProofTree → ReaderT ProofTree.RenderingP
       exportToGraph tree
       modify <| edges %~ (·.push { source := headId, target := mkGadgetId treeLoc })
 
-    modify ({· with location := state.location, xOffset := state.xOffset })
+    let yOffsetNew := max (state.yOffset + (← nodeSize term)) (← get).yOffset
+
+    modify ({· with location := state.location, xOffset := state.xOffset, yOffset := yOffsetNew })
 
     let gadgetProps : GadgetPropsWithPosition := {
       id := s!"gadget_{state.location}",
       inputs := goals.toArray.map ProofTree.headTerm,
       output? := some term,
       x := state.xOffset,
-      y := (state.yOffset + (← get).yOffset) / 2
+      y := (state.yOffset + yOffsetNew) / 2
     }
 
     modify <| gadgets %~ (·.push gadgetProps)
+
+def ProofTree.getGadgetGraph (proofTree : ProofTree) : (Array GadgetPropsWithPosition) × (Array Edge) :=
+  let (_, state) := proofTree.exportToGraph |>.run {} |>.run {}
+  (state.gadgets, state.edges)
 
 end GadgetGame
