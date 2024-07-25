5c2b7422994c0f6777e88a71bce33bf7e739a22b
Jonas Bayer <jonas.bayer999@gmail.com>
2024-07-25 12:57:33 +0200

    Fix: Inline display of handles

--- web-proof-machine/components/game/CustomHandle.tsx
+++ web-proof-machine/components/game/CustomHandle.tsx
@@ -1,15 +1,16 @@
-import { twMerge } from "tailwind-merge"
+import { twJoin } from "tailwind-merge"
 
 interface CustomHandleProps {
     type: "source" | "target"
-    isConnected: boolean
+    isConnected?: boolean
+    isInline?: boolean
 }
 
-export function CustomHandle(props: CustomHandleProps) {
-    const polygonPoints = props.type === "source" ? "3,2 14,2 19,10 14,18 3,18" : "10,10 5,2 17,2 22,10 17,18 5,18"
+export function CustomHandle({ type, isConnected = false, isInline = false }: CustomHandleProps) {
+    const polygonPoints = type === "source" ? "3,2 14,2 19,10 14,18 3,18" : "10,10 5,2 17,2 22,10 17,18 5,18"
 
-    return <svg className={twMerge("stroke-[1.5px] stroke-black pointer-events-none fill-white")} width="24" height="20" xmlns="http://www.w3.org/2000/svg">
+    return <svg className={twJoin("stroke-[1.5px] stroke-black pointer-events-none fill-white", isInline && "inline")} width="24" height="20" xmlns="http://www.w3.org/2000/svg">
         <polygon points={polygonPoints} />
-        {props.type === "target" && props.isConnected ? <polyline points="1,2 6,10 1,18" fill="none" /> : <></>}
+        {type === "target" && isConnected ? <polyline points="1,2 6,10 1,18" fill="none" /> : <></>}
     </svg>
 }
