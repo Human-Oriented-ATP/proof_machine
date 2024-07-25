5faf5472199b5270016967b0fb6ac67b38543769
Jonas Bayer <jonas.bayer999@gmail.com>
2024-07-25 14:18:01 +0200

    Feat: Help window closes when clicking on background

--- web-proof-machine/components/primitive/SingleButtonPopup.tsx
+++ web-proof-machine/components/primitive/SingleButtonPopup.tsx
@@ -11,8 +11,8 @@ const SingleButtonPopup: React.FC<SingleButtonPopupProps> = (props) => {
     return (
         <>
             {props.isOpen && (
-                <div className="fixed inset-0 flex items-center justify-center z-10 backdrop-blur-sm bg-white/30">
-                    <div className="bg-white p-6 rounded-lg shadow-lg">
+                <div className="fixed inset-0 flex items-center justify-center z-10 backdrop-blur-sm bg-white/30" onClick={props.close}>
+                    <div className="bg-white p-6 rounded-lg shadow-lg" onClick={e => e.stopPropagation()}>
                         <div>{props.children}</div>
                         <Button onClick={props.close}>Close</Button>
                     </div>
