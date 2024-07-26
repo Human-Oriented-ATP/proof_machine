76b77b873fa4f86d80b9a2127a8cfd967c89d78f
Jonas Bayer <jonas.bayer999@gmail.com>
2024-07-26 16:50:34 +0200

    Fix: Don't display next level button when there is no next level

--- web-proof-machine/components/primitive/MenuButtons.tsx
+++ web-proof-machine/components/primitive/MenuButtons.tsx
@@ -2,7 +2,6 @@ import Button, { HighlightedButton } from './Button';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import { useConfiguration, getNextProblem } from 'lib/study/LevelConfiguration';
-import { QuestionMarkIcon } from '@radix-ui/react-icons';
 
 function getProblemFromPathname(pathname: string): string {
     const pathComponents = pathname.split("/")
@@ -24,11 +23,11 @@ export function MenuButtons({ levelCompleted, showHelpWindow }: MenuButtonsProps
     function getNextLevelHref(): undefined | string {
         if (config) {
             const nextProblem = getNextProblem(config, currentProblem);
-            return `../game/${nextProblem}`;
+            if (nextProblem !== undefined) {
+                return `../game/${nextProblem}`;
+            }
         }
-        else {
-            return undefined;
-        }
+        return undefined;
     }
 
     function getStudyHomeHref(): string {
@@ -37,26 +36,21 @@ export function MenuButtons({ levelCompleted, showHelpWindow }: MenuButtonsProps
 
     const nextLevelHref = getNextLevelHref();
 
-    if (nextLevelHref !== undefined) {
-        return <>
-            <div className='m-1'>
-                <Button onClick={showHelpWindow}>Help</Button>
-            </div>
-            <div className='m-1'>
-                <Button onClick={() => router.push(getStudyHomeHref())}>Main menu</Button>
-            </div>
-            <div className='m-1'>
-                <Link href={nextLevelHref}>
-                    <HighlightedButton disabled={!levelCompleted}
-                        title={levelCompleted ? "" : "Connect all gadgets and remove broken connections to continue."}>
-                        Next level
-                    </HighlightedButton>
-                </Link>
-            </div>
-        </>;
-    } else {
-        return <div>
-            <HighlightedButton onClick={() => router.push(getStudyHomeHref())}>Main menu</HighlightedButton>
-        </div>;
-    }
+    return <>
+        <div className='m-1'>
+            <Button onClick={showHelpWindow}>Help</Button>
+        </div>
+        <div className='m-1'>
+            <Button onClick={() => router.push(getStudyHomeHref())}>Main menu</Button>
+        </div>
+        {nextLevelHref !== undefined && <div className='m-1'>
+            <Link href={nextLevelHref}>
+                <HighlightedButton disabled={!levelCompleted}
+                    title={levelCompleted ? "" : "Connect all gadgets and remove broken connections to continue."}>
+                    Next level
+                </HighlightedButton>
+            </Link>
+        </div>
+        }
+    </>;
 }
