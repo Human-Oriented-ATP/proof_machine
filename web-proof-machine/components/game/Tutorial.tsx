732f60769229a433de656976bcb4c40fea48001f
Jonas Bayer <jonas.bayer999@gmail.com>
2024-08-22 14:50:45 +0100

    Draft: Proof of concept tutorial

---
+++ web-proof-machine/components/game/Tutorial.tsx
@@ -0,0 +1,10 @@
+import { InitializationData } from "lib/game/Initialization";
+import { Game } from "./Game";
+
+interface TutorialProps {
+    initData: InitializationData
+}
+
+export function Tutorial(props: TutorialProps) {
+    return <Game {...props} setProblemSolved={() => console.log()} />;
+}
