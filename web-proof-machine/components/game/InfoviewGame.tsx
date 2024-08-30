721cc1ea73ca789b97d2e3b4145979978d6370f6
0art0 <18333981+0art0@users.noreply.github.com>
2024-08-30 18:41:47 +0100

    Include `return` in `InfoviewGame.tsx`

--- web-proof-machine/components/game/InfoviewGame.tsx
+++ web-proof-machine/components/game/InfoviewGame.tsx
@@ -2,5 +2,5 @@ import { InitializationData } from "lib/game/Initialization"
 import { Game } from "./Game"
 
 export default function InfoviewGame(initData: InitializationData) {
-    <Game initData={initData} setProblemSolved={() => { }} proximityConnectEnabled={true} />
+    return <Game initData={initData} setProblemSolved={() => { }} proximityConnectEnabled={true} />
 }
