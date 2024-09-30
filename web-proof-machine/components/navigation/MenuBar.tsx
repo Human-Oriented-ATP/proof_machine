ae68e1375114f6ec51c3a1c9ce9ba4c9cec66125
Jonas Bayer <jonas.bayer999@gmail.com>
2024-09-30 09:43:07 +0100

    Minor: Tweak status bar messages

--- web-proof-machine/components/navigation/MenuBar.tsx
+++ web-proof-machine/components/navigation/MenuBar.tsx
@@ -1,6 +1,6 @@
 import { StatusBarState } from 'components/game/GameScreen';
 import { MenuButtons } from './MenuButtons';
-import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
+import { ExclamationTriangleIcon, StarIcon } from '@radix-ui/react-icons';
 import { Connector } from 'components/game/gadget/Connector';
 
 interface LevelCompletedBannerProps {
@@ -8,13 +8,17 @@ interface LevelCompletedBannerProps {
     showHelpWindow: () => void
 }
 
-const LEVEL_COMPLETED_MESSAGE = <>Level completed!</>
-const BROKEN_CONNECTION_MESSAGE = <span className='text-center'>
+const LEVEL_COMPLETED_MESSAGE = <>
+    <StarIcon className='w-8 h-8 inline-block mr-2'/>
+    Level completed!
+    <StarIcon className='w-8 h-8 inline-block ml-2'/>
+    </>
+const BROKEN_CONNECTION_MESSAGE = <>
     <ExclamationTriangleIcon className='w-8 h-8 inline-block mr-2'/>
     Broken connection! Click 
     <span className="inline-block mx-0.5"><Connector type={'target'} isInline={true} isBroken={true} /></span> 
     to remove 
-    </span>
+    </>
 const EMPTY_MESSAGE = <></> 
 
 export default function MenuBar(props: LevelCompletedBannerProps) {
