41da0b693e989d80c25fd67ee738a34ea21e5d68
Jonas Bayer <jonas.bayer999@gmail.com>
2024-10-01 15:02:13 +0100

    Feat: Tutorial02 specification draft

--- web-proof-machine/components/tutorial/InteractiveTutorialLevels.tsx
+++ web-proof-machine/components/tutorial/InteractiveTutorialLevels.tsx
@@ -2,7 +2,7 @@ import { Connector } from "components/game/gadget/Connector";
 import { DragIndicatorProps, OverlayPosition } from "./DragIndicator";
 import { GadgetPosition, InteractiveLevel } from "./InteractiveLevel";
 import { InitialDiagram } from "lib/game/Initialization";
-import { parseAxiom, parseTerm } from "lib/parsing/Semantics";
+import { parseAxiom, parseStatement, parseTerm } from "lib/parsing/Semantics";
 
 function SourceConnector() { 
     return <div className="inline-block translate-y-[-2px]"><Connector type={"source"} isInline={true}/></div>
@@ -67,9 +67,59 @@ const tutorial01: InteractiveLevel = {
     }]
 }
 
+const tutorial02InitialDiagram : InitialDiagram = { 
+    gadgets: new Map([
+        ["goal_gadget", { statement: { goal: parseTerm("r(1, 2)") }, position: {x : -300, y: 0}}],
+        ["initial_gadget_1", { statement: { axiom: parseAxiom("r(2, 1).") }, position: {x : -600, y: -50}}],   
+    ]),
+    connections: [ {
+        from: "initial_gadget_1",
+        to: ["goal_gadget", 0]
+    } ]
+}
+
+const tutorial02DragIndicatorDeleteConnection : DragIndicatorProps<GadgetPosition> = {
+    origin: {
+        gadget: { elementId: "goal_gadget" },
+        anchorPoint: "CENTER_LEFT",
+        offset: { x: 0, y: 0}
+    },
+    destination: { relativePosition: { x: -30, y: 0 } },
+    drawLine: false,
+}
+
+const tutorial02DragIndicatorAddSwapGadget : DragIndicatorProps<GadgetPosition> = {
+    origin: {
+        gadget: { elementId: "axiom_2" },
+        anchorPoint: "BOTTOM_RIGHT",
+        offset: { x: -20, y: -20 }
+    },
+    destination: { relativePosition: { x: 200, y: 200 } },
+    drawLine: false,
+}
+
+
 const tutorial02: InteractiveLevel = {
     settings: RESTRICTIVE_SETTINGS,
-    steps: []
+    initialDiagram: tutorial02InitialDiagram, 
+    steps: [{
+        content: { 
+            jsx: <>hiya</>,
+            dragIndicator: tutorial02DragIndicatorDeleteConnection
+        },
+        trigger: { ConnectionRemoved: {} }
+    }, { 
+        content: {
+            jsx: <>Try using this gadget to swap the numbers</>,
+            dragIndicator: tutorial02DragIndicatorAddSwapGadget
+        },
+        trigger: { GadgetAdded: { axiom: "r(B, A):-r(A, B)"} } 
+    },
+    { 
+        content: { 
+            jsx: <>Well done!</>
+        }
+    }]
 }
 
 export const interactiveTutorialLevels: Map<string, InteractiveLevel> = new Map([
