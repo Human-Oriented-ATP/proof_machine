541ee321a45aea680b8fda4cf9cd770b203e1330
Jonas Bayer <jonas.bayer999@gmail.com>
2024-10-03 11:14:07 +0100

    Feat: Further pink hole graphics

--- web-proof-machine/components/tutorial/levels/tutorial08.tsx
+++ web-proof-machine/components/tutorial/levels/tutorial08.tsx
@@ -1,11 +1,11 @@
 import { InteractiveLevel } from "../InteractiveLevel";
-import { DEFAULT_SETTINGS, OpenTargetConnector } from "../TutorialSetup";
+import { DEFAULT_SETTINGS, PinkHole } from "../TutorialSetup";
 
 export const tutorial08: InteractiveLevel = {
     settings: DEFAULT_SETTINGS,
     steps: [{
         content: {
-            jsx: <>If the rest of the numbers in a cell are the same, ? becomes the same number. </>
+            jsx: <>If the rest of the numbers in a cell are the same, <PinkHole /> becomes the same number. </>
         },
         trigger: {
             GameCompleted: null
--- web-proof-machine/components/tutorial/levels/tutorial09.tsx
+++ web-proof-machine/components/tutorial/levels/tutorial09.tsx
@@ -1,11 +1,11 @@
 import { InteractiveLevel } from "../InteractiveLevel";
-import { DEFAULT_SETTINGS, OpenTargetConnector } from "../TutorialSetup";
+import { DEFAULT_SETTINGS, OpenTargetConnector, PinkHole } from "../TutorialSetup";
 
 export const tutorial09: InteractiveLevel = {
     settings: DEFAULT_SETTINGS,
     steps: [{
         content: {
-            jsx: <>The new numbers created by ?s behave normally, and can even occur in the other nodes of a ? gadget</>
+            jsx: <>The new numbers created by <PinkHole />s behave normally, and can even occur in the other nodes of a <PinkHole /> gadget</>
         },
         trigger: {
             GameCompleted: null
