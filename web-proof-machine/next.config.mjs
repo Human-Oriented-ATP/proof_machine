e96a83a0de5e60b281e1739118ef31b522967666
Jonas Bayer <jonas.bayer999@gmail.com>
2024-10-04 16:16:35 +0100

    Refactor: Remove experimental option from next.config file

--- web-proof-machine/next.config.mjs
+++ web-proof-machine/next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // distDir: "build",
-    experimental: {
-        nftTracing: true
-    }
 }
 
 export default nextConfig
