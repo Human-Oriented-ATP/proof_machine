a41da6cd872b32ea98cf8e28970751aeaa1787c5
Jonas Bayer <jonas.bayer999@gmail.com>
2024-10-04 11:35:56 +0100

    CI: Attempt using path.resolve instead of process.cwd

--- web-proof-machine/app/[config]/page.tsx
+++ web-proof-machine/app/[config]/page.tsx
@@ -3,6 +3,8 @@ import { promises as fs } from "fs"
 import { loadAllProblemsInDirectory } from "lib/game/LoadProblems";
 import { StudyConfiguration } from "lib/study/Types";
 
+const path = require('node:path'); 
+
 export async function generateStaticParams() {
     let configurations = ["pilot1", "pilot2", "new-tutorial"]
     const slugs = configurations.map(problem => ({ config: problem }))
@@ -10,7 +12,9 @@ export async function generateStaticParams() {
 }
 
 export default async function Page({ params }: { params: { config: string } }) {
-    const configuration : StudyConfiguration = JSON.parse(await fs.readFile(process.cwd() + "/study_setup/" + params.config + ".json", "utf-8"))
+    const filePath = path.resolve('study_setup', params.config + ".json")
+    const configurationFile = await fs.readFile(filePath, "utf-8")
+    const configuration : StudyConfiguration = JSON.parse(configurationFile)
     const allProblems = await loadAllProblemsInDirectory()
     
     return <StudyScreen config={configuration} allProblems={allProblems} />
