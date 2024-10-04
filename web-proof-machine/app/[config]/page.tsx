2b0aa3af83d9e9be9ef345aea766bae4faca89ac
Jonas Bayer <jonas.bayer999@gmail.com>
2024-10-04 09:45:28 +0100

    Fix: Navigation bugs

--- web-proof-machine/app/[config]/page.tsx
+++ web-proof-machine/app/[config]/page.tsx
@@ -5,13 +5,12 @@ import { StudyConfiguration } from "lib/study/Types";
 
 export async function generateStaticParams() {
     let configurations = ["pilot1", "pilot2", "new-tutorial"]
-    const slugs = configurations.map(problem => ({ slug: problem }))
+    const slugs = configurations.map(problem => ({ config: problem }))
     return slugs
 }
 
-export default async function Page({ params }: { params: { slug: string } }) {
-    const configurationIdentifier = params.slug
-    const configuration : StudyConfiguration = JSON.parse(await fs.readFile(process.cwd() + "/study_setup/" + configurationIdentifier + ".json", "utf-8"))
+export default async function Page({ params }: { params: { config: string } }) {
+    const configuration : StudyConfiguration = JSON.parse(await fs.readFile(process.cwd() + "/study_setup/" + params.config + ".json", "utf-8"))
     const allProblems = await loadAllProblemsInDirectory()
     
     return <StudyScreen config={configuration} allProblems={allProblems} />
