d3ba38063a4d54b3cb53ceee6e9a37cd57b51a6a
Jonas Bayer <jonas.bayer999@gmail.com>
2024-10-22 10:07:08 +0100

    Feat: Use state for rendering handles

--- web-proof-machine/components/game/gadget/CustomHandle.tsx
+++ web-proof-machine/components/game/gadget/CustomHandle.tsx
@@ -1,6 +1,7 @@
 import { Handle as ReactFlowHandle, Position } from "@xyflow/react"
 import { StaticHandle } from "./StaticHandle"
 import { Connector } from "./Connector"
+import { useGameStateContext } from "lib/state/StateContextProvider"
 
 export type CustomHandleProps = {
     type: "source" | "target"
@@ -8,13 +9,17 @@ export type CustomHandleProps = {
 }
 
 export function CustomHandle(props: CustomHandleProps) {
+    const handleStatus = useGameStateContext((state) => state.handleStatus)
+    const connectingHandles = useGameStateContext((state) => state.connectingHandles)
     if (props.handleId === undefined) {
         return <StaticHandle {...props} />
     }
     else {
         const position = props.type === "source" ? Position.Right : Position.Left
+        const status = handleStatus.get(props.handleId)
+        const isConnecting = connectingHandles.includes(props.handleId)
         return <ReactFlowHandle type={props.type} position={position} id={props.handleId}>
-            <Connector type={props.type} />
+            <Connector type={props.type} status={status} isConnecting={isConnecting} />
         </ReactFlowHandle>
     }
 }
