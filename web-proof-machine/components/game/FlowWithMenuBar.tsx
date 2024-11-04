b95869888266ef7540b727361538569aeb621c1d
Jonas Bayer <jonas.bayer999@gmail.com>
2024-11-04 14:15:49 +0000

    Refactor: Remove old code

--- web-proof-machine/components/game/FlowWithMenuBar.tsx
+++ web-proof-machine/components/game/FlowWithMenuBar.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useCallback, useState } from "react";
 import MenuBar from "components/navigation/MenuBar";
 import { Flow } from "./flow/Flow";
 import { GadgetShelf } from "./flow/GadgetShelf";
@@ -8,16 +7,6 @@ import { HelpPopup } from "./HelpPopup";
 import { InteractiveOverlay } from "components/tutorial/InteractiveOverlay";
 
 export default function FlowWithMenuBar() {
-    const markLevelAsCompleted = useCallback(() => {
-        // setLevelIsCompleted(true)
-        // saveLevelCompletedAsCookie(props.problemId)
-    }, [])
-
-    // const interactiveLevel = interactiveTutorialLevels.get(props.problemId)
-    // const initialDiagram = interactiveLevel?.initialDiagram
-    // const initData = initialDiagram ? { ...props.initData, initialDiagram } : props.initData
-    // const settings = interactiveLevel?.settings
-
     return <div className='h-dvh flex flex-col'>
         <div className="w-full"><MenuBar /></div>
         <div className="relative flex flex-1">
