c87b7d94d9280d7c96304da9c4e5a5a58979a6ec
Jonas Bayer <jonas.bayer999@gmail.com>
2024-11-05 12:01:18 +0000

    Dev: Enable zustand developer tools

--- web-proof-machine/lib/state/Store.ts
+++ web-proof-machine/lib/state/Store.ts
@@ -1,6 +1,7 @@
 import { createStore } from 'zustand'
 import { FlowSlice, flowSlice, FlowStateInitializedFromData } from './slices/Flow'
 import { HelpPopupSlice, helpPopupSlice } from './slices/HelpPopup'
+import { devtools } from 'zustand/middleware'
 
 export type GameStateInitializedFromData = FlowStateInitializedFromData
 export type GameSlice = FlowSlice & HelpPopupSlice
@@ -8,9 +9,9 @@ export type GameSlice = FlowSlice & HelpPopupSlice
 export type GameStore = ReturnType<typeof createGameStore>
 
 export const createGameStore = (initialState: GameStateInitializedFromData) => {
-    return createStore<GameSlice>()((set, get) => ({
+    return createStore<GameSlice>()(devtools((set, get) => ({
         ...initialState,
         ...flowSlice(initialState, set, get),
         ...helpPopupSlice(set),
-    }))
+    })))
 }
