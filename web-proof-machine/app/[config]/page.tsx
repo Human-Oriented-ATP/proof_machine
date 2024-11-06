1cd9b399bc16973fe0aa204853b7c0ea67f2104a
Jonas Bayer <jonas.bayer999@gmail.com>
2024-11-06 09:52:47 +0000

    Feat: Add internal page

--- web-proof-machine/app/[config]/page.tsx
+++ web-proof-machine/app/[config]/page.tsx
@@ -1,8 +1,9 @@
+import MainScreen from "components/navigation/MainScreen";
 import StudyScreen from "components/navigation/StudyScreen";
 import { loadAllProblemsInDirectory, loadStudyConfiguration } from "lib/game/LoadProblems";
 
 export async function generateStaticParams() {
-    let configurations = ["pilot1", "pilot2", "new-tutorial"]
+    let configurations = ["internal", "pilot1", "pilot2", "new-tutorial"]
     const slugs = configurations.map(problem => ({ config: problem }))
     return slugs
 }
@@ -10,6 +11,10 @@ export async function generateStaticParams() {
 export default async function Page({ params }: { params: { config: string } }) {
     const configuration = await loadStudyConfiguration(params.config)
     const allProblems = await loadAllProblemsInDirectory()
-    
-    return <StudyScreen config={configuration} allProblems={allProblems} />
+
+    if (params.config === "internal") {
+        return <MainScreen allProblems={allProblems} />
+    } else {
+        return <StudyScreen config={configuration} allProblems={allProblems} />
+    }
 }
