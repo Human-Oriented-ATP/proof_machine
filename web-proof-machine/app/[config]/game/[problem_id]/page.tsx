f2c6cbc9e70634c218c776b7d49abcae15a18f1a
Jonas Bayer <jonas.bayer999@gmail.com>
2024-11-16 09:19:42 +0000

    Debug: Remove questionnaire checks in game page

--- web-proof-machine/app/[config]/game/[problem_id]/page.tsx
+++ web-proof-machine/app/[config]/game/[problem_id]/page.tsx
@@ -13,24 +13,24 @@ export async function generateStaticParams() {
 
 export default async function Page({ params }: { params: { config: string, problem_id: string } }) {
     const configuration = await loadStudyConfiguration(params.config)
-    if (params.problem_id === "questionnaire1") {
-        if (await hasSubmittedQuestionnaire1()) {
-            redirect("./tim_easy10")
-        } else {
-            return <Questionnaire1 />
-        }
-    } else if (params.problem_id === "questionnaire2") {
-        if (await hasSubmittedQuestionnaire2()) {
-            redirect("../")
-        } else {
-            return <Questionnaire2 />
-        }
-    } else {
-        if (await progressSufficientForQuestionnaire2(configuration)) {
-            redirect("./questionnaire2")
-        } else {
-            const game = await GameLoader({ problemId: params.problem_id, configId: params.config })
-            return game
-        }
-    }
+    // if (params.problem_id === "questionnaire1") {
+    //     if (await hasSubmittedQuestionnaire1()) {
+    //         redirect("./tim_easy10")
+    //     } else {
+    //         return <Questionnaire1 />
+    //     }
+    // } else if (params.problem_id === "questionnaire2") {
+    //     if (await hasSubmittedQuestionnaire2()) {
+    //         redirect("../")
+    //     } else {
+    //         return <Questionnaire2 />
+    //     }
+    // } else {
+    //     if (await progressSufficientForQuestionnaire2(configuration)) {
+    //         redirect("./questionnaire2")
+    //     } else {
+    const game = await GameLoader({ problemId: params.problem_id, configId: params.config })
+    return game
+    // }
+    // }
 }
