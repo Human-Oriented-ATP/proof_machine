599e611079c199308f11c651c31e428a47ffe0fd
Jonas Bayer <jonas.bayer999@gmail.com>
2024-11-18 12:10:45 +0000

    Feat: Enable Vercel Analytics

--- web-proof-machine/app/layout.tsx
+++ web-proof-machine/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next"
 import "../index.css"
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
     title: 'Gadgets Game',
@@ -13,11 +14,12 @@ export default function RootLayout({
 }) {
     return <html lang="en">
         <head>
-        <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/favicon.ico" />
         </head>
         <body className="bg-light-gray">
             <noscript>You need to enable JavaScript to run this app.</noscript>
             <div id="root">{children}</div>
+            <Analytics />
         </body>
     </html>
 }
