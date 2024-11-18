796aa12c25053ef654bdb29c25830a359c1bff9b
Jonas Bayer <jonas.bayer999@gmail.com>
2024-11-18 12:50:42 +0000

    Feat: Button for ending participation

--- web-proof-machine/components/navigation/ProblemSelection.tsx
+++ web-proof-machine/components/navigation/ProblemSelection.tsx
@@ -1,5 +1,7 @@
+import Button from "components/primitive/buttons/Default"
 import { StudyConfiguration } from "lib/study/Types"
 import dynamic from "next/dynamic"
+import Link from "next/link"
 
 const DynamicProblemCategoryGrid = dynamic(() => import("./ProblemGrid"), { ssr: false })
 const DynamicStartButton = dynamic(() => import("../primitive/buttons/StartFirstUnsolvedLevel"), { ssr: false })
@@ -10,5 +12,10 @@ export function ProblemSelection(props: { config: StudyConfiguration, allProblem
             <DynamicProblemCategoryGrid {...props} />
         </div>
         <DynamicStartButton config={props.config} />
+        <div className="relative md:fixed bottom-0 left-0 p-2">
+            <Link href={`${props.config.name}/game/questionnaire2`}>
+                <Button moreClassnames="text-sm">End participation in study</Button>
+            </Link >
+        </div>
     </>
 }
