c6ad61f3cb2eb2c14f7c90cc182bea4a1add1c74
Jonas Bayer <jonas.bayer999@gmail.com>
2024-12-10 08:17:54 +0100

    Feat: Skip button UI

--- web-proof-machine/components/navigation/MenuButtons.tsx
+++ web-proof-machine/components/navigation/MenuButtons.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation';
 import { useGameStateContext } from 'lib/state/StateContextProvider';
 import { useShallow } from 'zustand/react/shallow';
 import { GameSlice } from 'lib/state/Store';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const selector = (state: GameSlice) => ({
     levelIsCompleted: state.levelIsCompleted,
@@ -13,15 +13,26 @@ const selector = (state: GameSlice) => ({
     reset: state.reset,
     uploadHistory: state.uploadHistory,
     isTutorialLevel: state.setup.settings.isTutorialLevel,
+    skipTime: state.setup.settings.skipTime,
 })
 
+function formatTime(seconds: number): string {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    const paddedSeconds = remainingSeconds.toString().padStart(2, "0");
+    return `${minutes}:${paddedSeconds}`;
+}
+
 export function MenuButtons() {
-    const { levelIsCompleted, openHelpPopup, reset, uploadHistory, isTutorialLevel } = useGameStateContext(useShallow(selector))
+    const { levelIsCompleted, openHelpPopup, reset, uploadHistory, isTutorialLevel, skipTime } = useGameStateContext(useShallow(selector))
 
     const router = useRouter();
 
     const { nextProblem } = useGameStateContext((state) => state.setup)
     const nextLevelHref = nextProblem ? `../game/${nextProblem}` : undefined
+
+    const showMenuButton = skipTime === null && !isTutorialLevel
+    const showSkipButton = skipTime !== null && nextLevelHref !== undefined
 
     const restartLevel = useCallback(() => {
         const confirmed = confirm("Are you sure that you want to restart the level? All progress will be lost.")
@@ -36,18 +47,40 @@ export function MenuButtons() {
         router.push('../')
     }, [])
 
+    const [timeUntilSkip, setTimeUntilSkip] = useState(skipTime || 0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (timeUntilSkip <= 0) {
+                clearInterval(interval)
+                return
+            }
+            setTimeUntilSkip((prev) => prev - 1)
+        }, 1000)
+    }, [skipTime])
+
     return <>
         <div className='m-1'>
             <Button onClick={openHelpPopup}>Help</Button>
         </div>
-        {!isTutorialLevel && <>
+        {showMenuButton &&
             <div className='m-1'>
                 <Button onClick={mainButtonAction}>Main menu</Button>
             </div>
+        }
+        {!isTutorialLevel &&
             <div className='m-1'>
                 <Button onClick={restartLevel}>Restart level</Button>
             </div>
-        </>
+        }
+        {showSkipButton && <div className='m-1'>
+            <Link href={nextLevelHref}>
+                <Button disabled={timeUntilSkip > 0}
+                    title={levelIsCompleted ? "" : `You can skip this level in ${formatTime(timeUntilSkip)}.`}>
+                    Skip level
+                </Button>
+            </Link>
+        </div>
         }
         {nextLevelHref !== undefined && <div className='m-1'>
             <Link href={nextLevelHref}>
--- web-proof-machine/components/primitive/buttons/Default.tsx
+++ web-proof-machine/components/primitive/buttons/Default.tsx
@@ -1,7 +1,8 @@
 import { twJoin } from "tailwind-merge"
 
 export default function Button({ highlightOnHover = true, type = undefined, moreClassnames = "", ...props }) {
-    return <button className={twJoin("border-2 border-black rounded-lg p-2.5", highlightOnHover && "hover:bg-black hover:text-white", moreClassnames)}
+    return <button className={twJoin("border-2 border-black rounded-lg p-2.5 disabled:bg-palette-gray disabled:opacity-40 disabled:cursor-not-allowed",
+        highlightOnHover && "hover:bg-black hover:enabled:text-white", moreClassnames)}
         {...props}>
         {props.children}
     </button>
