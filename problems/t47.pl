0c6b28e3627f69a7e6523c8e02ef9e66b06348d1
wtgowers <wtg10@cam.ac.uk>
2025-02-12 09:26:52 +0000

    Add files via upload

--- problems/t47.pl
+++ problems/t47.pl
@@ -4,9 +4,8 @@ r(1,5).
 r(2,3).
 r(2,4).
 r(3,4).
-r(C,B) :- r(A,B), r(A,C).
 b(B,C) :- r(A,B), r(A,C).
 r(A,B) :- r(A,C), r(B,C).
-r(A,C) :- r(A,B), b(B,C).
+r(C,A) :- r(A,B), b(B,C).
 :- r(4,3).
 
