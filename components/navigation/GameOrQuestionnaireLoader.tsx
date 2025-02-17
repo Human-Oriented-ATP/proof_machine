098fbf5eb3e4fe68e651f1db219396fa743d49f0
Jonas Bayer <jonas.bayer999@gmail.com>
2025-02-17 11:18:02 +0000

    Config: Disable questionnaire

--- components/navigation/GameOrQuestionnaireLoader.tsx
+++ components/navigation/GameOrQuestionnaireLoader.tsx
@@ -6,16 +6,16 @@ import { StudyConfiguration } from "lib/study/Types";
 import { redirect } from "next/navigation";
 
 export default async function GameOrQuestionnaireLoader({ problemId, config }:
-    { problemId: string, config: StudyConfiguration }) {
-    if (problemId === "questionnaire1") {
-        return <Questionnaire1 redirectTo="./tutorial01" />
-    } else if (problemId === "questionnaire2") {
-        return <Questionnaire2 redirectTo={"../thanks"} />
-    } else {
-        if (await progressSufficientForQuestionnaire2(config)) {
-            redirect("./questionnaire2")
-        }
-        const game = await GameLoader({ problemId, configId: config.name })
-        return game
-    }
+  { problemId: string, config: StudyConfiguration }) {
+  if (problemId === "questionnaire1") {
+    return <Questionnaire1 redirectTo="./tutorial01" />
+  } else if (problemId === "questionnaire2") {
+    return <Questionnaire2 redirectTo={"../thanks"} />
+  } else {
+    // if (await progressSufficientForQuestionnaire2(config)) {
+    //     redirect("./questionnaire2")
+    // }
+    const game = await GameLoader({ problemId, configId: config.name })
+    return game
+  }
 }
