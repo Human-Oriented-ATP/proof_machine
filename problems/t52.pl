6778c7e48cba1c9e12da8500b3028b7d8ac265d1
wtgowers <wtg10@cam.ac.uk>
2025-02-22 23:48:23 +0000

    Add files via upload

--- problems/t52.pl
+++ problems/t52.pl
@@ -9,4 +9,6 @@ g(B,X,C) :- g(A,X,Y), g(B,A,Z), g(C,Y,Z).
 g(B,A,Z) :- g(A,X,Y), g(B,X,C), g(C,Y,Z).
 g(X,A,W) :- g(A,X,Y), b(W,Y).
 b(A,B) :- b(B,A).
+o(C) :- b(A,B), g(C,A,B).
+g(X,X,C) :- o(C).
 :- g(6,4,5).
