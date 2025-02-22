ec0fc9827591ecac30c8d232adc7809b309be493
wtgowers <wtg10@cam.ac.uk>
2025-02-22 11:07:09 +0000

    Add files via upload

--- problems/t49.pl
+++ problems/t49.pl
@@ -8,7 +8,9 @@ r(1,5).
 r(2,6).
 r(3,7).
 g(A,B,C) :- g(C,A,B).
+r(A,B) :- r(B,A).
 b(A,B) :- g(A,B,C), g(A,B,D), r(C,D).
+b(B,C) :- b(A,B), b(D,C), r(A,D).
 w(B) :- b(A,B), b(C,B), r(A,C).
 :- w(4).
 
