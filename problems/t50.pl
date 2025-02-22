43472b49da40db1823b87adbb9b08b18033d7619
wtgowers <wtg10@cam.ac.uk>
2025-02-22 16:38:25 +0000

    Add files via upload

--- problems/t50.pl
+++ problems/t50.pl
@@ -7,6 +7,7 @@ g(7,6,5).
 r(xr(A),A).
 b(xb(A,B),A,B).
 g(xg(A,B),A,B).
+g(A,B,C) :- g(A,C,B).
 y(A,B) :- b(A,X,Y), r(B,X).
 g(A,B,C) :- w(Z,X,Y), r(A,Z), r(B,Y), b(C,X,Y).
 y(A,D) :- g(A,B,C), g(D,B,E), y(C,E).
