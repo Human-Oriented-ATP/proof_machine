34fa61613a60a13854e92988e02defd17401dd05
wtgowers <wtg10@cam.ac.uk>
2025-02-22 20:01:56 +0000

    Add files via upload

--- problems/t51.pl
+++ problems/t51.pl
@@ -7,5 +7,6 @@ g(xg(A,B),A,B).
 r(W,Z) :- b(W,X,Y), y(Z,X,Y).
 b(W,X,Y) :- r(W,Z), y(Z,X,Y). 
 o(W,T) :- r(U,X), r(V,Y), r(W,Z), g(Z,X,Y), g(T,U,V).
-y(C,X,Z) :- y(A,X,Y), y(B,Y,Z), y(C,A,B).
+y(C,X,Z) :- y(A,X,Y), y(B,Y,Z), g(C,A,B).
+g(C,A,B) :- y(A,X,Y), y(B,Y,Z), y(C,X,Z).
 :- o(6,7).
