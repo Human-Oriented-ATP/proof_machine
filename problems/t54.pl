b6c7e8f990064eb7c75f67b8cfb635cf71498909
wtgowers <wtg10@cam.ac.uk>
2025-02-24 19:28:21 +0000

    Add files via upload

--- problems/t54.pl
+++ problems/t54.pl
@@ -1,9 +1,11 @@
 g(1,1,2,3).
 g(A,B,C,D) :- g(A,C,B,D).
 g(B,A,B,C) :- g(A,A,B,C).
-g(C,C,B,A) :- g(A,B,A,C).
-g(A,B,C,B) :- g(A,B,C,A).
+g(C,C,B,A) :- g(A,B,C,A).
+g(A,B,C,B) :- g(C,B,A,A).
 g(B,C,A,B) :- g(B,A,A,C).
 g(A,C,C,B) :- g(B,A,C,A).
-g(A,A,B,C) :- g(B,C,A,A).
+g(A,B,C,C) :- g(B,C,B,A).
 :- g(1,1,3,2).
+
+
